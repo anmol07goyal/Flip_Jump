b95116bd678b18317c5adb81529012d3b3645341
Anmol Goyal <anmol77goyal@gmail.com>
2025-09-09 20:10:19 -0600

    roads not moving issue fixed

--- Assets/Scripts/BgMovingHandler.cs
+++ Assets/Scripts/BgMovingHandler.cs
@@ -3,23 +3,30 @@ using UnityEngine;
 public class BgMovingHandler : MonoBehaviour
 {
     [SerializeField] private Material _bgMaterial;
+    [SerializeField] private Material _roadMaterial;
+
     [SerializeField] private float _scrollSpeed = 0.5f;
 
-    private Vector2 _textureOffset;
+    private Vector2 _bgTextureOffset;
+    private Vector2 _roadTextureOffset;
 
     private void Start()
     {
-        _textureOffset = _bgMaterial.mainTextureOffset;
+        _bgTextureOffset = _bgMaterial.mainTextureOffset;
+        _roadTextureOffset = _roadMaterial.mainTextureOffset;
     }
     
     private void Update()
     {
-        _textureOffset.x += Time.deltaTime * _scrollSpeed;
+        _bgTextureOffset.x += Time.deltaTime * _scrollSpeed;
+        _roadTextureOffset.x += Time.deltaTime * _scrollSpeed;
 
         // Use the modulo operator to wrap the offset between 0 and 1
         // This prevents the value from growing indefinitely
-        _textureOffset.x %= 1.0f;
+        _bgTextureOffset.x %= 1.0f;
+        _roadTextureOffset.x %= 1.0f;
 
-        _bgMaterial.mainTextureOffset = _textureOffset;
+        _bgMaterial.mainTextureOffset = _bgTextureOffset;
+        _roadMaterial.mainTextureOffset = _roadTextureOffset;
     }
 }
